e149375d65b90c74f9a205d084dba4ec32b9b40f
Tom Mott <thomasnmott1997@gmail.com>
2023-07-13 10:57:06 +0100

    Redesign

--- js/index.js
+++ js/index.js
@@ -2,38 +2,38 @@ window.onload = init;
 
 function init() {
 	createMagic();
-	for (let j of document.querySelectorAll('.background-inner')) {j.addEventListener("mouseover", colorMagic)};
+	for (let j of document.querySelectorAll(".absolute")) {
+		j.addEventListener("mouseover", colorMagic);
+	}
 }
 
 function createMagic() {
+	for (i = 16; i < 84; i += 1) {
+		for (j = 24; j <= 84; j += 2) {
+			const div = document.createElement("div");
+			div.classList.add("absolute");
+			const innerDiv = document.createElement("div");
 
-	for (i = 8; i < 92; i += 2) {
+			div.appendChild(innerDiv);
+			// innerDiv.classList.add("bg-white");
+			innerDiv.classList.add("w-4");
+			innerDiv.classList.add("h-4");
 
-		for(j = 12; j <= 88; j += 4) {
+			div.style.left = `${i}%`;
+			div.style.top = `${j}%`;
 
-			if(( j >= 76 || j <= 24) || ( i >= 68 || i <= 28)) {
-				const div = document.createElement("div");
-				div.classList.add("background-inner");
-				const innerDiv = document.createElement("div");
+			var color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+			div.style.backgroundColor = color;
 
-				div.appendChild(innerDiv);
-				innerDiv.classList.add("background-magic");
-				innerDiv.classList.add("circle-s");
-
-				div.style.left = `${i}%`;
-				div.style.top = `${j}%`;
-
-				document.getElementById("background").appendChild(div);
-			}
-
+			document.getElementById("background").appendChild(div);
 		}
 	}
 }
 
 function colorMagic() {
 	var magic = this.childNodes[0];
-	var color = '#'+Math.floor(Math.random()*16777215).toString(16);
+	var color = "#" + Math.floor(Math.random() * 16777215).toString(16);
 	// var color = '#fccd6b';
 
 	magic.style.backgroundColor = color;
-};
+}
